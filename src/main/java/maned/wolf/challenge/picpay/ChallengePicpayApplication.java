271e8138f70cedfbb835afc783255e637fdfece7
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-06-24 21:15:15 -0300

    Initial commit

---
+++ src/main/java/maned/wolf/challenge/picpay/ChallengePicpayApplication.java
@@ -0,0 +1,13 @@
+package maned.wolf.challenge.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengePicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengePicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maned/wolf/challenge/picpay/ChallengePicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package maned.wolf.challenge.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengePicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
